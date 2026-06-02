c4bc7267fb2539fa5565bfd85183696572dd7380
legolas0630 <carlosfcruces@gmail.com>
2026-06-02 11:21:31 -0600

    fix: force dynamic evaluation on music api and update build workspace tracking variables

--- .next/static/chunks/app/logout/successful/page-a8286c2a0f43fba5.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[19,996],{3077:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,997,744],function(){return n(n.s=3077)}),_N_E=n.O()}]);
